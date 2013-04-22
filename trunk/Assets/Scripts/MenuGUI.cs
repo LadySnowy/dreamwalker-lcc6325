4f74b5a0a6cfcd32663b883ae1de329291b2fa0e
justin.permar@gmail.com <justin.permar@gmail.com@a59e7a6f-e6f9-4a3a-75d8-9752bb2e1a49>
2013-04-22 14:23:33 +0000

    Fixed a bug in which the cursor wasn't displayed in the menu scene.

--- trunk/Assets/Scripts/MenuGUI.cs
+++ trunk/Assets/Scripts/MenuGUI.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
 using System.Collections;
 
-public class MenuGUI : MonoBehaviour {
+public class MenuGUI : MonoBehaviour
+{
 	
 
 	public Texture mouse;
 	
-	void OnGUI () {
-			var width = Screen.width;
-	var height = Screen.height;
+	// Update is called once per frame
+	void Update () {
+		Screen.showCursor = true;
+	}
+	
+	void OnGUI ()
+	{
+		var width = Screen.width;
+		var height = Screen.height;
 		
 		GUI.skin.box.fontSize = 40;
 		GUI.skin.button.fontSize = 20;
@@ -18,9 +25,9 @@ public class MenuGUI : MonoBehaviour {
 		GUIStyle style = new GUIStyle ();
 		style.fontSize = 36;
 		style.normal.textColor = new Color (1.0f, 1.0f, 1.0f, 1.0f);
-		GUIContent content = new GUIContent("Dreamwalker");
-		Rect titleRect = GUILayoutUtility.GetRect(content, style);
-		Rect placedRect = new Rect(width/2 - titleRect.width/2, titleRect.height, titleRect.width, titleRect.height);
+		GUIContent content = new GUIContent ("Dreamwalker");
+		Rect titleRect = GUILayoutUtility.GetRect (content, style);
+		Rect placedRect = new Rect (width / 2 - titleRect.width / 2, titleRect.height, titleRect.width, titleRect.height);
 		GUI.Label (placedRect, content, style);
 		//GUI.Box(new Rect(width/2-250,50,500,height-100), "");
 		
@@ -28,24 +35,24 @@ public class MenuGUI : MonoBehaviour {
 		//GUI.TextArea(new Rect(width/2-200, 100, 400, 140),text);
 		
 		// CONTROLS
-		GUI.Label(new Rect(width/2-40, 240, 100, 50), "Controls");
+		GUI.Label (new Rect (width / 2 - 40, 240, 100, 50), "Controls");
 		GUI.enabled = false;
-		GUI.Button(new Rect(width/2-125, 270, 45,45), "W");
-		GUI.Button(new Rect(width/2-175, 320, 45,45), "A");
-		GUI.Button(new Rect(width/2-75, 320, 45,45), "D");
-		GUI.Button(new Rect(width/2-125, 370, 45,45), "S");
+		GUI.Button (new Rect (width / 2 - 125, 270, 45, 45), "W");
+		GUI.Button (new Rect (width / 2 - 175, 320, 45, 45), "A");
+		GUI.Button (new Rect (width / 2 - 75, 320, 45, 45), "D");
+		GUI.Button (new Rect (width / 2 - 125, 370, 45, 45), "S");
 		GUI.enabled = true;
 		
-		GUI.DrawTexture(new Rect(width/2+50, 320, 50,50), mouse, ScaleMode.ScaleToFit);
-		GUI.Label(new Rect(width/2+100, 320, 125,200), "Move mouse to look");
+		GUI.DrawTexture (new Rect (width / 2 + 50, 320, 50, 50), mouse, ScaleMode.ScaleToFit);
+		GUI.Label (new Rect (width / 2 + 100, 320, 125, 200), "Move mouse to look");
 		
-		GUI.Label(new Rect(width/2-175, 420, 400, 40), "Press h for a hint");
+		GUI.Label (new Rect (width / 2 - 175, 420, 400, 40), "Press h for a hint");
 
-		GUI.Label(new Rect(width/2-175, 460, 400, 100), "To interact with something, walk into it.");
+		GUI.Label (new Rect (width / 2 - 175, 460, 400, 100), "To interact with something, walk into it.");
 		
 		// START GAME
-		if(GUI.Button(new Rect(width/2-75,height-100,150,50), "Start Game")) {
-			Application.LoadLevel("world");
+		if (GUI.Button (new Rect (width / 2 - 75, height - 100, 150, 50), "Start Game")) {
+			Application.LoadLevel ("world");
 			//PlayerScript.Health = 0;
 		}
 	}
